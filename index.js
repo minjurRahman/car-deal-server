5818404d9e05746bb5eb39b34bf5521703ed9981
Minjur <minjur.rahaman99@gmail.com>
2022-11-25 14:13:51 +0600

    connect to database

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -12,27 +14,33 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.bjaguop.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bjaguop.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+  try{
 
 
+  }
+  catch(error){
+    console.log(error.name, error.message.bold, error.stack)
+  }
+  finally{
+
+  }
+
+
+}
+
+run()
 
 
 
 app.get('/', async(req, res) =>{
     res.send('Car Deal server is running');
 })
-
-
-
-
-
-
 
 
 
