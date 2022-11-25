74a8fd84fb336de9ce1ce87368c8a8ea4c0fee34
Minjur <minjur.rahaman99@gmail.com>
2022-11-25 14:23:46 +0600

    get category route

--- index.js
+++ index.js
@@ -12,16 +12,21 @@ app.use(cors());
 app.use(express.json());
 
 
-
-
+//Connect To Database
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bjaguop.mongodb.net/?retryWrites=true&w=majority`;
 
-console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+//CRUD operation function
 async function run(){
   try{
+    const categoryCollection = client.db('CarDeal').collection('category');
 
+    app.get('/category', async(req, res) =>{
+      const query = {};
+      const options = await categoryCollection.find(query).toArray();
+      res.send(options);
+    })
 
   }
   catch(error){
