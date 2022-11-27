28307832cd66efc55935186c88dac1c62ba8b0d3
Minjur <minjur.rahaman99@gmail.com>
2022-11-27 16:50:26 +0600

    dsf

--- index.js
+++ index.js
@@ -136,7 +136,7 @@ async function run(){
     app.get('/users/admin/:email', async(req, res) =>{
       const email = req.params.email;
       const query = { email: email }
-      const user = await usersCollections.findOne(query);
+      const user = await usersCollection.findOne(query);
       res.send({isAdmin: user?.role === 'admin'});
     })
 
