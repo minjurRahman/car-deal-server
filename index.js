afb632ba80ab73382564415ac3fe956f10714281
Minjur <minjur.rahaman99@gmail.com>
2022-11-27 10:42:04 +0600

    token implement

--- index.js
+++ index.js
@@ -88,6 +88,20 @@ async function run(){
     })
 
 
+    //JWT token
+    app.get('/jwt', async(req, res) =>{
+      const email = req.query.email;
+      const query = {email: email};
+      const user = await usersCollection.findOne(query);
+      if(user){
+        const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn: '7d'})
+        return res.send({accessToken: token})
+      }
+      console.log(user);
+      res.status(403).send({accessToken: ''});
+    });
+
+
   }
   catch(error){
     console.log(error.name, error.message.bold, error.stack)
